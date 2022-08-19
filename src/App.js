213cfa8ae9550194497d5d42d58e554c6d186374
MelloWeb3 <amakkieh@gmail.com>
2022-08-19 15:15:46 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+
+import { Checkbox, FormGroup, FormControlLabel } from '@mui/material';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [allChecked, setAllChecked] = useState(false);
+  const [checkedValues, setCheckedValues] = useState([]);
+  const [kosherChecked, setKosherChecked] = useState(false);
+  const [eggsChecked, setEggsChecked] = useState(false);
+  const [dairyChecked, setDairyChecked] = useState(false);
+  
+  const itemList = [ "Kosher", "No Eggs", "No Dairy"]
+
+  const handleAllChange = () => {
+    // allChecked === false ? () =>
+    setAllChecked(!allChecked)
+    setKosherChecked(!kosherChecked);
+    setEggsChecked(!eggsChecked);
+    setDairyChecked(!dairyChecked)
+};
+
+  const checkHandler = (event) => {
+    const targetLabel = event.target.label
+    const targetChecked = event.target.checked
+    if (targetChecked) {
+      setCheckedValues([...checkedValues, targetLabel])
+    }
+  }
+  const handleKosherChange = () => {
+    setKosherChecked(!kosherChecked);
+  };
+
+  const handleEggsChange = () => {
+    setEggsChecked(!eggsChecked);
+  };
+  const handleDairyChange = () => {
+    setDairyChecked(!dairyChecked);
+  };
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+    <div> Selected Value: {checkedValues.map(value => 
+    value
+    )}</div>
+      <FormGroup className='App'> 
+      <FormControlLabel className='checkBox' control={<Checkbox onClick={handleAllChange} size='large'/>} label="Select All"/>
+      <FormControlLabel className='checkBox' control={<Checkbox onChange={checkHandler} onClick={handleKosherChange} checked={kosherChecked} size='large'/>} label="Kosher"/>
+      <FormControlLabel className='checkBox' control={<Checkbox onChange={checkHandler} onClick={handleEggsChange} checked={eggsChecked}  size='large'/>} label="No Eggs"/>
+      <FormControlLabel className='checkBox' control={<Checkbox onChange={checkHandler} onClick={handleDairyChange} checked={dairyChecked} size='large'/>} label="No Dairy"/>
+      </FormGroup>
+      </>
   );
 }
 
